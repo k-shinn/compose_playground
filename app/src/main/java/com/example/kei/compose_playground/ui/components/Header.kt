b592a2a74417bc1004ce25af3bcc1eb8611b0b9a
k-shinn <k.shinn0721@gmail.com>
2022-01-23 17:36:23 +0900

    ToolTipの試作中

--- app/src/main/java/com/example/kei/compose_playground/ui/components/Header.kt
+++ app/src/main/java/com/example/kei/compose_playground/ui/components/Header.kt
@@ -7,15 +7,19 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.GenericShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -23,6 +27,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.example.kei.compose_playground.R
 import com.example.kei.compose_playground.model.DemoContent
 import com.example.kei.compose_playground.ui.theme.Compose_playgroundTheme
@@ -55,31 +60,55 @@ fun StickyMenu(
     onClickD: () -> Unit,
 ) {
     Row {
-        StickyMenuContents(
-            id = android.R.drawable.ic_dialog_info,
-            text = stringResource(R.string.sticky_menu_a),
-            onClick = onClickA
-        )
-        StickyMenuContents(
-            id = android.R.drawable.ic_dialog_alert,
-            text = stringResource(R.string.sticky_menu_b),
-            onClick = onClickB
-        )
-        StickyMenuContents(
-            id = android.R.drawable.ic_menu_add,
-            text = stringResource(R.string.sticky_menu_c),
-            onClick = onClickC
-        )
-        StickyMenuContents(
-            id = android.R.drawable.ic_menu_help,
-            text = stringResource(R.string.sticky_menu_d),
-            onClick = onClickD
-        )
+        Box(
+            modifier = Modifier
+                .wrapContentHeight()
+                .weight(weight = 1f)
+        ) {
+            StickyMenuContents(
+                id = android.R.drawable.ic_dialog_info,
+                text = stringResource(R.string.sticky_menu_a),
+                onClick = onClickA
+            )
+        }
+        Box(
+            modifier = Modifier
+                .wrapContentHeight()
+                .weight(weight = 1f)
+        ) {
+            StickyMenuContentsWithBalloon(
+                id = android.R.drawable.ic_dialog_alert,
+                text = stringResource(R.string.sticky_menu_b),
+                onClick = onClickB
+            )
+        }
+        Box(
+            modifier = Modifier
+                .wrapContentHeight()
+                .weight(weight = 1f)
+        ) {
+            StickyMenuContents(
+                id = android.R.drawable.ic_menu_add,
+                text = stringResource(R.string.sticky_menu_c),
+                onClick = onClickC
+            )
+        }
+        Box(
+            modifier = Modifier
+                .wrapContentHeight()
+                .weight(weight = 1f)
+        ) {
+            StickyMenuContents(
+                id = android.R.drawable.ic_menu_help,
+                text = stringResource(R.string.sticky_menu_d),
+                onClick = onClickD
+            )
+        }
     }
 }
 
 @Composable
-fun RowScope.StickyMenuContents(
+fun StickyMenuContents(
     @DrawableRes id: Int,
     text: String,
     onClick: () -> Unit
@@ -88,10 +117,7 @@ fun RowScope.StickyMenuContents(
         modifier = Modifier
             .clickable(onClick = onClick)
             .wrapContentHeight()
-            .weight(
-                weight = 1f,
-                fill = true
-            )
+            .fillMaxWidth()
             .background(color = Color.DarkGray)
             .padding(bottom = 4.dp, top = 4.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -104,6 +130,59 @@ fun RowScope.StickyMenuContents(
         Text(
             text = text,
             fontSize = 12.sp
+        )
+    }
+}
+
+@Composable
+fun StickyMenuContentsWithBalloon(
+    @DrawableRes id: Int,
+    text: String,
+    onClick: () -> Unit
+) {
+    Box {
+        StickyMenuContents(id = id, text = text, onClick = onClick)
+        Box(
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .offset(0.dp, -(30.dp)) // TODO: 適切なOffset値(Balloon自体のサイズ)を取得設定する方法が分からない
+        ) {
+            Balloon(message = "test")
+        }
+    }
+}
+
+@Composable
+fun Balloon(
+    message: String
+) {
+    val tailShape = GenericShape { size, _ ->
+        lineTo(size.width, 0f)
+        lineTo(size.width / 2, size.height)
+    }
+
+    Column {
+        Box(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .clip(RoundedCornerShape(16.dp))
+                .background(color = Color.Red)
+        ) {
+            Text(
+                text = message,
+                color = Color.White,
+                modifier = Modifier.padding(
+                    horizontal = 8.dp,
+                    vertical = 2.dp
+                )
+            )
+        }
+        Box(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .size(10.dp)
+                .clip(tailShape)
+                .background(color = Color.Red)
         )
     }
 }
@@ -132,5 +211,14 @@ fun PreviewHeaderStickyMenu() {
     val onclick = {}
     Compose_playgroundTheme {
         StickyMenu(onclick, onclick, onclick, onclick)
+    }
+}
+
+
+@Preview
+@Composable
+fun PreviewBalloon() {
+    Compose_playgroundTheme {
+        Balloon(message = "バルーン表示テスト")
     }
 }
